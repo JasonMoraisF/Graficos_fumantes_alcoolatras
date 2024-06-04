9a00e33889f2f29a626b785915e7c38970ddf063
Keitty Carla Cardoso Gomes <1241304073@acd.grupotiradentes.com>
2024-06-04 13:06:48 -0300

    Feat: Modelo aplicado, porem nao funcional

--- fbGraph/app.py
+++ fbGraph/app.py
@@ -3,8 +3,15 @@ from warnings import filterwarnings
 import pickle
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
 
+def import_model():  # abre o modelo treinado
+    modelo = pickle.load(open('./fbGraph/modelo.sav', 'rb'))
+    return modelo
+
+
+modelo = import_model()
+
+app = Flask(__name__)
 
 
 @app.route('/') #importando o template
@@ -12,9 +19,39 @@ def index():
     return render_template('index.html')
 
 @app.route('/resposta_index',methods=['GET']) #importando o template
-def predict():
+def respostaPag():
     return render_template('resposta_index.html')
 
+@app.route('/predict',methods=['POST']) #mandando seus dados
+def predict():
+     
+    parametros = [float(request.form['genero']),
+        float(request.form['Idade']), 
+    float(request.form['Peso']), 
+    float(request.form['Cintura']), 
+    float(request.form['Colesterol_Total']), 
+    float(request.form['PAS']), 
+    float(request.form['PAD']),
+    float(request.form['BLDS']), 
+    float(request.form['HDL_colesterol']), 
+    float(request.form['Triglicerídio']), 
+    float(request.form['Hemoglobina']), 
+    float(request.form['Proteina_da_urina']),
+    float(request.form['soro_creatinina']), 
+    float(request.form['SGOT_AST']), 
+    float(request.form['SGOT_ALT']), 
+    float(request.form['gamma_GTP']), 
+    float(request.form['Voce fuma'])]
+    
+    resultado = modelo.predict([ parametros ])[0]
+    
+    if resultado == 0: 
+        resultado = 'Você não é alcóolatra'
+    else:
+        resultado = 'Você é alcóolatra'
+
+
+    return render_template('resposta_index.html', resultado=resultado)
 
 
 if __name__ == '__main__':
